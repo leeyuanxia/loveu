310c37694483eb34ed37a9b189d723875a8569c5
leeyuanxia <1152557928@qq.com>
2021-09-04 22:51:58 +0800

    '【Loveu】【l-1】第一次提交'

---
+++ app/src/main/java/com/zcgc/loveu/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.zcgc.loveu.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.zcgc.loveu.R;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
